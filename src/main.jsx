5dbd773d40419a33e2859545927230a4ec2f4e40
Mayuri Atole <atolemayuri17@gmail.com>
2025-12-30 19:24:05 +0530

    Add index.css import

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { router } from "./routes/Router";
 import { RouterProvider } from "react-router-dom";
-import "../src/../index.css";
+import { router } from "./routes/Router.jsx";
+import "../index.css";  // 👈 ADD THIS!
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
