5d230dd7b421d824270a7ae1749ecc211cfb53b2
Mayuri Atole <atolemayuri17@gmail.com>
2025-12-30 18:12:50 +0530

    Remove missing CSS import

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,22 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-  
   return {
-    base: '/portfolio-website/',  // 👈 GitHub Pages repo name
+    base: `/${env.VITE_REPO_NAME}/`,
     plugins: [react(), tailwindcss()],
-    build: {
-      outDir: 'dist'
-    },
     server: {
-      open: true
-    }
+      open: true,
+      host: false, // Allow access from network devices
+      proxy: {
+        "/api": {
+          target: "http://localhost:5000",
+          changeOrigin: true,
+          secure: true,
+        },
+      },
+    },
   };
 });
